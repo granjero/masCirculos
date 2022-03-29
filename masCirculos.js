dbd4969a4d774dec5a8d94dc6fc6f8a09e35429b
juan miguel <juanmiguel@synergy.local>
2022-03-28 21:04:08 -0300

    colores

--- masCirculos.js
+++ masCirculos.js
@@ -1,10 +1,12 @@
 let circulos = [];
 let contador = 0;
+let paleta;
 
 function setup() {
-    colorMode(HSL);
+    colorMode(HSB);
     createCanvas(windowWidth, windowHeight);
     ellipseMode(RADIUS);
+    paleta = new Paleta(random(360));
 }
 
 function draw() {
@@ -12,7 +14,7 @@ function draw() {
     background(50);
     noStroke();
     fill(52, 19, 70);
-    rect(width * 0.025, height * 0.025, width * 0.95, height * 0.95);
+    rect(30, 30, width - 60, height - 60);
     noFill();
     //fill(random(360), random(50, 100), 54);
     //stroke(255);
@@ -23,7 +25,7 @@ function draw() {
         contador = 0;
     } else {
         contador++;
-        console.log(contador);
+        //console.log(contador);
         if (contador > 1000) {
             noLoop();
             console.log("FIN");
@@ -31,8 +33,8 @@ function draw() {
     }
     for (let i = 0; i < circulos.length; i++) {
         circulos[i].dibuja();
-        //circulos[i].crece();
     }
+    chequeaCrecimiento();
 }
 
 // Circulo / null
@@ -43,8 +45,25 @@ function nuevoCirculo() {
         random(height * 0.05, height * 0.95)
     );
     let valido = true;
-    valido = nuevoCirculoValido(centro);
+    valido = esElNuevoCirculoValido(centro);
 
+    return valido
+        ? new Circulo(centro.x, centro.y, 1, random(paleta.colores()))
+        : null;
+}
+
+// boolean
+function esElNuevoCirculoValido(centroNuevoCirculo) {
+    for (let circulo of circulos) {
+        if (centroNuevoCirculo.dist(circulo.centro) <= circulo.r + 10) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// void
+function chequeaCrecimiento() {
     for (let circulo of circulos) {
         for (let otroCirculo of circulos) {
             if (circulo != otroCirculo) {
@@ -58,29 +77,82 @@ function nuevoCirculo() {
             }
         }
     }
-
-    return valido ? new Circulo(centro.x, centro.y, 1) : null;
 }
 
-function nuevoCirculoValido(centroNuevoCirculo) {
-    for (let circulo of circulos) {
-        if (centroNuevoCirculo.dist(circulo.centro) <= circulo.r + 10) {
-            return false;
-        }
+class Paleta {
+    constructor(hue) {
+        this.hue = floor(hue);
     }
-    return true;
+
+    // int 0 - 360
+    hueMasAngulo(angulo) {
+        let resultante = floor(angulo + this.hue);
+        //console.log(resultante);
+        return resultante <= 360 ? resultante : resultante - 360;
+    }
+
+    // color HSB
+    colorPrimario() {
+        let color = [];
+        color.push(random(this.hue - 5, this.hue + 5));
+        color.push(random(50, 100));
+        color.push(random(35, 45));
+        //console.log(color);
+        return color;
+    }
+
+    // color HSB
+    colorSecundario() {
+        let color = [];
+        color.push(random(this.hue + 15, this.hue + 25));
+        color.push(random(50, 100));
+        color.push(random(35, 45));
+        //console.log(color);
+        return color;
+    }
+
+    // color HSB
+    colorTerciario() {
+        let color = [];
+        color.push(random(this.hue - 15, this.hue - 25));
+        color.push(random(50, 100));
+        color.push(random(35, 45));
+        //console.log(color);
+        return color;
+    }
+
+    // color HSB
+    colorOpuesto() {
+        let color = [];
+        color.push(random(this.hueMasAngulo(170), this.hueMasAngulo(190)));
+        color.push(random(50, 100));
+        color.push(random(35, 45));
+        //console.log(color);
+        return color;
+    }
+
+    // array colores HSB
+    colores() {
+        let colores = [];
+        colores.push(this.colorPrimario());
+        colores.push(this.colorSecundario());
+        colores.push(this.colorTerciario());
+        colores.push(this.colorOpuesto());
+        return colores;
+    }
 }
 
 class Circulo {
     creciendo;
-    constructor(x, y, r) {
+    constructor(x, y, r, color) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.centro = createVector();
         this.centro.set(x, y);
         this.creciendo = true;
-        this.colorRelleno = [random(360), random(50, 100), 54];
+        //this.colorRelleno = [random(360), random(50, 100), 40];
+        this.colorRelleno = color;
     }
 
     // display
